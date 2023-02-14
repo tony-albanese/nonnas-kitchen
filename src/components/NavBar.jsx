03de368fa8215d419940316b459b178dc0c750d5
tony-albanese <tony.albanese2000@gmail.com>
2023-02-14 18:16:34 +0000

    Add style module to NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-
 import { NavLink } from "react-router-dom";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top">
       <Container>
-        <NavLink to="/">
+        <NavLink to="/" className={styles.NavLink}>
           <Navbar.Brand>Nonna's Kitchen</Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink exact to="/">
+            <NavLink exact to="/" className={styles.NavLink}>
               Home
             </NavLink>
-            <NavLink to="/signin">Sign in</NavLink>
-            <NavLink to="/signup">Sign up</NavLink>
+            <NavLink to="/signin" className={styles.NavLink}>Sign in</NavLink>
+            <NavLink to="/signup" className={styles.NavLink}>Sign up</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
