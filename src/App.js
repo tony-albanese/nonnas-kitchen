d4ff4b495607526be229263581f08a23e68f357a
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 07:21:00 +0000

    Add boilerplate to SignUpForm and linked to nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/pages/auth/SignUpForm.jsx
+++ src/pages/auth/SignUpForm.jsx
@@ -1,1 +1,24 @@
 //The SignUpForm component will go here.
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+const SignUpForm = () => {
+  return (
+    <Row>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container>
+          <h1>Sign Up</h1>
+          {/* add your form here */}
+        </Container>
+        <Container className={`mt-3`}>
+          <Link to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
