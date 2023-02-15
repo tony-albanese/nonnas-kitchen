51c558c53e96098ff1bdb86693fa50bdc4591751
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 08:27:21 +0000

    Add form control elements username and password.

--- src/pages/auth/SignInForm.jsx
+++ src/pages/auth/SignInForm.jsx
@@ -1,32 +1,39 @@
 import React from "react";
-import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
 
 export default function SignInForm() {
   return (
     <Row>
       <Col md={6}>
-      <Container>
-      <h1>Sign In</h1>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <Container>
+          <h1>Sign In</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control type="text" placeholder="Username" />
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Sign In
+            </Button>
+          </Form>
+
+          <Form.Text className="text-muted">
+            Don't have an account? Sign Up.
+          </Form.Text>
         </Container>
       </Col>
     </Row>
