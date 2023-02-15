17106812a27389d41043ac64d8a5a417ed06711d
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 13:15:00 +0000

    Add conditional rendering of nav components.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
+
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInNavItems = <>{currentUser?.username}</>
+  const loggedOutNavItems = (
+    <>
+      <NavLink to="/signin" className={styles.NavLink}>
+        Sign in
+      </NavLink>
+      <NavLink to="/signup" className={styles.NavLink}>
+        Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar expand="md" fixed="top">
       <Container>
@@ -16,8 +31,7 @@ const NavBar = () => {
             <NavLink exact to="/" className={styles.NavLink}>
               Home
             </NavLink>
-            <NavLink to="/signin" className={styles.NavLink}>Sign in</NavLink>
-            <NavLink to="/signup" className={styles.NavLink}>Sign up</NavLink>
+            {currentUser ?loggedInNavItems : loggedOutNavItems}
           </Nav>
         </Navbar.Collapse>
       </Container>
