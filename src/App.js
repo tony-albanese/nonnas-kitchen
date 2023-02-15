5755cf892fcba704f0494632908ea6e02a7b83de
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 14:48:37 +0000

    Move handleMount, useEffect, and state

--- src/App.js
+++ src/App.js
@@ -12,20 +12,7 @@ export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
 
-  const handleMount = async () => {
-    try { 
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-     }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,3 +1,16 @@
-export const CurrentUserProvider = ({children}) => {
-    
+export const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const handleMount = async () => {
+        try {
+            const { data } = await axios.get("dj-rest-auth/user/");
+            setCurrentUser(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        handleMount();
+    }, []);
 };
