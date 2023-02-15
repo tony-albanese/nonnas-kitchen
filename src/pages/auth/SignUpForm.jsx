22b97a04a5e9ac65adbcf29227effb0f2cbd7500
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 07:28:00 +0000

    Setup form fields for signup

--- src/pages/auth/SignUpForm.jsx
+++ src/pages/auth/SignUpForm.jsx
@@ -9,7 +9,38 @@ const SignUpForm = () => {
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container>
           <h1>Sign Up</h1>
-          {/* add your form here */}
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3`}>
           <Link to="/signin">
