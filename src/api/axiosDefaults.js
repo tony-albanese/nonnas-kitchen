f72e85101a60c010ed69a75a1381d18a5e3b6bbc
tony-albanese <tony.albanese2000@gmail.com>
2023-02-15 19:14:07 +0000

    Add axios interceptors for request and response

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://nonnas-kitchen-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosRequest = axios.create();
+export const axiosResponse = axios.create();
