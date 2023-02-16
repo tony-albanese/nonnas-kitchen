9037cfceec9b0e036ca0571a99e7e486a1c41c2f
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 15:58:17 +0000

    Add hook to fetch current post on mount

--- src/pages/posts/PostPage.jsx
+++ src/pages/posts/PostPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Form,
     Button,
@@ -10,8 +10,32 @@ import {
     FormText,
     FormControl,
   } from "react-bootstrap";
+import { useParams } from 'react-router-dom';
+import { axiosRequest } from "../../api/axiosDefaults";
 
 export default function PostPage() {
+
+  const {id} = useParams();
+  const [blogPost, setBlogPost] = useState({results: []});
+
+  useEffect(()=>{
+    const handleMount = async () => {
+      try {
+        const [{data: post}] = await Promise.all([
+          axiosRequest.get(`/posts/${id}`),
+        ]);
+        setBlogPost({results: [post]});
+        console.log(post);
+
+      } catch (err){
+        console.log(err);
+      }
+    }
+
+    handleMount();
+  },[id]
+  ) ;
+
   return (
     <Row className="h-100">
         <Col lg={8}>
