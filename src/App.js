1091582abe47eb3d62f3809a01e358f417775bfc
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 08:46:29 +0000

    Add route to BlogPostCreateEdit form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import BlogPostCreateForm from "./pages/auth/BlogPostCreateForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route exact path="/posts/create" render={() => <BlogPostCreateForm/>} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
