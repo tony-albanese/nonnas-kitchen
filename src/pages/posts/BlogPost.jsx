74fc623df707157a8ca653935a724f0df501f87f
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 17:08:07 +0000

    Add data to Header, Img, and Body

--- src/pages/posts/BlogPost.jsx
+++ src/pages/posts/BlogPost.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import styles from "../../styles/BlogPost.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Card,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Media,
+} from "react-bootstrap";
 
 const BlogPost = (props) => {
   const {
@@ -19,32 +27,28 @@ const BlogPost = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === author;
 
-  const categories = [
-    { anec: "Anecdote" },
-    { tip: "Tip" },
-    { hist: "History" },
-    { fact: "Fun Fact" },
-    { orig: "Origin" },
-    { remin: "Reminiscence" },
-  ];
-
+  const categories = {
+    anec: "Anecdote",
+    tip: "Tip",
+    hist: "History",
+    fact: "Fun Fact",
+    orig: "Origin",
+    remin: "Reminiscence",
+  };
 
   return (
     <Card style={{ width: "18rem" }}>
-        <Card.Header>
-            First Header
-        </Card.Header>
-        <Card.Header>
-            {author}
-            <span>{posted_on}</span>
-            {is_owner && postPage  && "   ..."}
-        </Card.Header>
+      <Card.Header>{categories[category]}</Card.Header>
+      <Media className="align-items-center justify-content-between">
+        {author}
+        <span>{posted_on}</span>
+        {is_owner && postPage && "..."}
+      </Media>
       <Card.Img variant="top" src={post_image} alt={title} />
+
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>
-         {body}
-        </Card.Text>
+        <Card.Text>{body}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">Some footer text.</Card.Footer>
     </Card>
