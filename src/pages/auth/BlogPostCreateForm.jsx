25755852d211bbc6148f9a9dd419e2a07f19c806
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 08:59:31 +0000

    Outline structure of form page.

--- src/pages/auth/BlogPostCreateForm.jsx
+++ src/pages/auth/BlogPostCreateForm.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Form,
+    Button,
+    Image,
+    Col,
+    Row,
+    Container,
+    Alert,
+    FormText,
+  } from "react-bootstrap";
 
 function BlogPostCreateForm() {
+
+const [errors, setErrors] = useState();
+const formFields = (
+    <div>Form fields will go here.</div>
+)
+
   return (
-    <div>BlogPostCreateForm</div>
+    <Form>
+        <Row>
+            <Col>
+            <Container>
+                <Form.Group>
+                    <Form.Label>
+                        
+                    </Form.Label>
+                </Form.Group>
+                <div className="d-md-none">{formFields}</div>
+            </Container>
+            </Col>
+            <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container>{formFields}</Container>
+        </Col>
+        </Row>
+    </Form>
   )
 }
 
