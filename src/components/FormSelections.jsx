43a53cba7a119a444ad1eb488041fc72682e78d9
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 10:42:13 +0000

    Reformat document.

--- src/components/FormSelections.jsx
+++ src/components/FormSelections.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {Form, FormControl} from "react-bootstrap";
-const FormSelections = ({controlName, onChangeHandler}) => {
+import React from "react";
+import { Form, FormControl } from "react-bootstrap";
 
-    return (
-        <Form.Control as="select" name={controlName} onChange={onChangeHandler}>
-       <option value="a">Anecdote</option>
-          <option value="b">Tip</option>
-          <option value="c">History</option>
-          <option value="d">Fun Fact</option>
-          <option value="e">Origin</option>
-          <option value="f">Reminiscence</option>
-        </Form.Control>
-    );
+
+const FormSelections = ({ controlName, onChangeHandler, options }) => {
+  return (
+    <Form.Control as="select" name={controlName} onChange={onChangeHandler}>
+      <option value="a">Anecdote</option>
+      <option value="b">Tip</option>
+      <option value="c">History</option>
+      <option value="d">Fun Fact</option>
+      <option value="e">Origin</option>
+      <option value="f">Reminiscence</option>
+    </Form.Control>
+  );
 };
 
 export default FormSelections;
-
