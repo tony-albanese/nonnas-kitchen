a882ea7d8d3bcbb7967edeedc594a7e70d6e4db8
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 16:33:58 +0000

    Destructure props. Determine is_owner.

--- src/pages/posts/BlogPost.jsx
+++ src/pages/posts/BlogPost.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import styles from "../../styles/BlogPost.module.css";
+import {useCurrentUser} from ".../../contexts/CurrentUserContext";
 
 const BlogPost = (props) => {
+const {
+id,
+author,
+title,
+body,
+category,
+posted_on,
+post_image,
+is_author,
+} = props;
+
+const currentUser = useCurrentUser();
+const is_owner = currentUser?.username === author;
   return (
     <div>BlogPost Component Placeholder</div>
   )
