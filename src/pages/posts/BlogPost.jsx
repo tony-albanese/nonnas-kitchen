06f72ece21f7a23fe7c75cd7a72e8063612f4cf2
tony-albanese <tony.albanese2000@gmail.com>
2023-02-16 16:25:38 +0000

    Initial commit.

---
+++ src/pages/posts/BlogPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/BlogPost.module.css";
+
+const BlogPost = (props) => {
+  return (
+    <div>BlogPost Component Placeholder</div>
+  )
+}
+
+export default BlogPost
