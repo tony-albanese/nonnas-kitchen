a03153c6df318f085e8f438dcd810de261637fb4
tony-albanese <tony.albanese2000@gmail.com>
2023-02-17 06:31:47 +0000

    Add icons to PostFooterContent

--- src/components/PostFooterContent.jsx
+++ src/components/PostFooterContent.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 
+
 export default function PostFooterContent({is_owner, is_liked, user, likes_count}) {
   return (
     <>
     <div>Post Footer Content</div>
-    {
-      is_owner ? (
-        <p>Is owner of the post</p>
-      ) : is_liked ? (<p>Owner likes this post</p>) : user? (<p>They are logged in.</p>): (<p>Sign in to like.</p>)
-    }
-    {likes_count}
+    <i class="fa-regular fa-heart"></i>
+    <i class="fa-solid fa-heart"></i>
+    <i class="fa-regular fa-comments"></i>
     </>
   )
 }
