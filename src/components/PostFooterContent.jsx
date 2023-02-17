9ff203177981172ad6e86457266fb1f6a1e84af5
tony-albanese <tony.albanese2000@gmail.com>
2023-02-17 07:21:12 +0000

    Add logic for icon behavior.

--- src/components/PostFooterContent.jsx
+++ src/components/PostFooterContent.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React from "react";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 
+export default function PostFooterContent({ isOwner, isLiked, loggedInUser }) {
+  const cantLikeOwnPostToolTip = <Tooltip>Can't like own post!</Tooltip>;
+  const logInPromptToolTip = <Tooltip>Please log in to like.</Tooltip>;
 
-export default function PostFooterContent({is_owner, is_liked, user, likes_count}) {
+  const iconPostIsOwner = (
+    <OverlayTrigger placement="top" overlay={cantLikeOwnPostToolTip}>
+      <i className="fa-regular fa-heart"></i>
+    </OverlayTrigger>
+  );
+
+  const iconPostIsLiked = (
+    <span onClick={() => {}}>
+      <i className="fa-solid fa-heart"></i>{" "}
+    </span>
+  );
+
+  const iconUserIsLoggedIn = (
+    <span onClick={() => {}}>
+      <i className="fa-regular fa-heart"></i>{" "}
+    </span>
+  );
+
+  const iconUserNotLoggedIn = (
+    <OverlayTrigger placement="top" overlay={logInPromptToolTip}>
+      <i class="fa-regular fa-heart"></i>
+    </OverlayTrigger>
+  );
+
   return (
-    <>
-    <div>Post Footer Content</div>
-    <i class="fa-regular fa-heart"></i>
-    <i class="fa-solid fa-heart"></i>
-    <i class="fa-regular fa-comments"></i>
-    </>
-  )
+    <Card.Footer className="text-muted">
+      <div>Post Footer Content</div>
+
+      {isOwner
+        ? iconPostIsOwner
+        : isLiked
+        ? iconPostIsLiked
+        : loggedInUser
+        ? iconUserIsLoggedIn
+        : iconUserNotLoggedIn}
+    </Card.Footer>
+  );
 }
