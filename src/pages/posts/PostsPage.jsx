6612485d8a391e7c990613bae30c0a3cdddb438e
tony-albanese <tony.albanese2000@gmail.com>
2023-02-17 10:55:38 +0000

    Implement useEffect hook to get posts.

--- src/pages/posts/PostsPage.jsx
+++ src/pages/posts/PostsPage.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import {
     Col,
     Row,
     Container,
   } from "react-bootstrap";
-function PostsPage(props) {
+import { useLocation } from 'react-router-dom';
+import { axiosRequest } from "../../api/axiosDefaults";
+
+
+function PostsPage({message, filter=""}) {
+
+    const [posts, setPosts] = useState({results: []});
+    const [dataLoaded, setDataLoaded] = useState(false);
+    const pathname = useLocation();
+
+    useEffect(
+        ()=>{
+            const getPosts = async () => {
+                try {
+                    const {data} = await axiosRequest.get(`/posts/?${filter}`)
+                    
+                }catch (err){
+                    console.log(err);
+                }
+
+            };
+            getPosts();
+        },
+        [filter, pathname]
+    );
+
   return (
     <Row className='h100'>
     <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>List of posts here</p>
-        <p>{props.message}</p>
+        <p>{message}</p>
       </Col>
 
     </Row>
