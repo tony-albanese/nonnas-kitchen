cda611dc023948d51c74a934aac63f1bc892ccea
tony-albanese <tony.albanese2000@gmail.com>
2023-02-18 15:05:00 +0000

    Refactor parameters for custom modal alert.

--- src/components/ModalAlert.jsx
+++ src/components/ModalAlert.jsx
@@ -2,24 +2,22 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function ModalAlert({show,  handleCancel}) {
+function ModalAlert({show,  handleClose, onConfirm}) {
   return (
-    <>
-    <Modal show={show} >
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCancel}>
-          Close
-        </Button>
-        <Button variant="primary">
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </>
+    <Modal show={show} onHide={handleClose} onClose={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Modal heading</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={onConfirm}>
+        Confirm Delete
+      </Button>
+    </Modal.Footer>
+  </Modal>
   )
 }
 
