8debe0b7f54327af639b162276ee35d34f1de36d
tony-albanese <tony.albanese2000@gmail.com>
2023-02-19 11:12:18 +0000

    Passed setPost and setComments callbacks to CommentCreateForm

--- src/pages/posts/PostPage.jsx
+++ src/pages/posts/PostPage.jsx
@@ -14,15 +14,20 @@ export default function PostPage() {
   const currentUser = useCurrentUser();
   const {id} = useParams();
   const [post, setPost] = useState({results: []});
+  const [comments, setComments] = useState({results: []});
+
+  const profile_image = currentUser?.profile_image;
 
 
   useEffect(()=>{
     const handleMount = async () => {
       try {
-        const [{data: post}] = await Promise.all([
+        const [{data: post}, {data: comments}] = await Promise.all([
           axiosRequest.get(`/posts/${id}`),
+          axiosRequest.get(`/comments/?blog_post=${id}`),
         ]);
         setPost({results: [post]});
+        setComments(comments);
 
       } catch (err){
         console.log(err);
@@ -35,14 +40,17 @@ export default function PostPage() {
 
   return (
     <Row className="h-100">
-        <Col lg={8}>
-            <BlogPost {...post.results[0]} setPosts={setPost} postPage />
-            <Container>
-                
-                {currentUser ? (<CommentCreateForm postId={id}/>) : (<p>Comments Go Here if there are any.</p>)}
-                
-            </Container>
-        </Col>
+      <Col lg={8}>
+        <BlogPost {...post.results[0]} setPosts={setPost} postPage />
+        <Container>
+          {currentUser ? (
+            <CommentCreateForm postId={id} setPost={setPost} setComments={setComments}
+             />
+          ) :null
+        }
+        {<p>Number of comments: {comments.results.length}</p>}
+        </Container>
+      </Col>
     </Row>
-  )
+  );
 }
