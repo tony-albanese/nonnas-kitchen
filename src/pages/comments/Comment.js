4b0e585e46a1bdf40d68085feef312521a44656f
tony-albanese <tony.albanese2000@gmail.com>
2023-02-19 12:12:00 +0000

    Add ModalAlert to Comment Add outline of event handlers to Comment.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Card,
     Row
 } from "react-bootstrap";
+import CardEdit from '../../components/CardEdit';
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import ModalAlert from "../../components/ModalAlert";
 
 function Comment({author, created_on, body}) {
+    const [show, setShow] = useState(false);
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === author;
+
+    const showConfirmDeleteModal = (event) => {
+        setShow(true);
+      };
+
+    const handleEdit = (event) => {
+        console.log("Edit the comment.");
+    };
+
+    const handleDelete = () => {
+        console.log("Delete the comment.")
+    };
+
     return (
+        <>
         <Card style={{ width: "20rem" }}>
             <Card.Body>
+                {is_owner && (<CardEdit onDelete={showConfirmDeleteModal} onEdit={handleEdit}/>)}
                 <Row>
                     <span>{author}</span>
                     <span>{created_on}</span>
-                    <span>...Icons if Owner</span>
                 </Row>
                 <Row>
                     <p>{body}</p>
@@ -19,6 +39,12 @@ function Comment({author, created_on, body}) {
             </Card.Body>
 
         </Card>
+        <ModalAlert
+        show={show}
+        handleClose={() => setShow(false)}
+        onConfirm={handleDelete}
+      />
+      </>
     )
 }
 
