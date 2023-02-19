452a2113435285e8749333c80e45fa4e82c1859b
tony-albanese <tony.albanese2000@gmail.com>
2023-02-19 11:57:53 +0000

    Implement basic Comment component.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {
+    Card,
+    Row
+} from "react-bootstrap";
 
-function Comment() {
-  return (
-    <div>Comment Component</div>
-  )
+function Comment({author, created_on, body}) {
+    return (
+        <Card style={{ width: "20rem" }}>
+            <Card.Body>
+                <Row>
+                    <span>{author}</span>
+                    <span>{created_on}</span>
+                    <span>...Icons if Owner</span>
+                </Row>
+                <Row>
+                    <p>{body}</p>
+                </Row>
+            </Card.Body>
+
+        </Card>
+    )
 }
 
 export default Comment
