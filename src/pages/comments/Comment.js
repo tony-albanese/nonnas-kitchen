ad7b4ee227ad3fb522209feca2c82f3fbbf09bed
tony-albanese <tony.albanese2000@gmail.com>
2023-02-19 12:37:57 +0000

    Update comments state after delete.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -8,14 +8,14 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import ModalAlert from "../../components/ModalAlert";
 import { axiosResponse } from '../../api/axiosDefaults';
 
-function Comment({id, author, created_on, body, setPost}) {
+function Comment({ id, author, created_on, body, setPost, setComments }) {
     const [show, setShow] = useState(false);
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === author;
 
     const showConfirmDeleteModal = (event) => {
         setShow(true);
-      };
+    };
 
     const handleEdit = (event) => {
         console.log("Edit the comment.");
@@ -27,13 +27,18 @@ function Comment({id, author, created_on, body, setPost}) {
             await axiosResponse.delete(`/comments/${id}/`);
             setPost((prevPost) => ({
                 results: [
-                  {
-                    ...prevPost.results[0],
-                    comments_count: prevPost.results[0].comments_count - 1,
-                  },
+                    {
+                        ...prevPost.results[0],
+                        comments_count: prevPost.results[0].comments_count - 1,
+                    },
                 ],
-              }));
-        }catch (err){
+            }));
+
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id),
+            }));
+        } catch (err) {
             console.log(err);
         }
         setShow(false);
@@ -41,25 +46,25 @@ function Comment({id, author, created_on, body, setPost}) {
 
     return (
         <>
-        <Card style={{ width: "20rem" }}>
-            <Card.Body>
-                {is_owner && (<CardEdit onDelete={showConfirmDeleteModal} onEdit={handleEdit}/>)}
-                <Row>
-                    <span>{author}</span>
-                    <span>{created_on}</span>
-                </Row>
-                <Row>
-                    <p>{body}</p>
-                </Row>
-            </Card.Body>
+            <Card style={{ width: "20rem" }}>
+                <Card.Body>
+                    {is_owner && (<CardEdit onDelete={showConfirmDeleteModal} onEdit={handleEdit} />)}
+                    <Row>
+                        <span>{author}</span>
+                        <span>{created_on}</span>
+                    </Row>
+                    <Row>
+                        <p>{body}</p>
+                    </Row>
+                </Card.Body>
 
-        </Card>
-        <ModalAlert
-        show={show}
-        handleClose={() => setShow(false)}
-        onConfirm={handleDelete}
-      />
-      </>
+            </Card>
+            <ModalAlert
+                show={show}
+                handleClose={() => setShow(false)}
+                onConfirm={handleDelete}
+            />
+        </>
     )
 }
 
