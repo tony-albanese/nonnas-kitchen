06cc6ab0fe34fb6e423635f43c42fda9f6ba1aab
tony-albanese <tony.albanese2000@gmail.com>
2023-02-22 08:39:04 +0000

    Add and style floating action button.

--- src/pages/recipes/RecipesPage.jsx
+++ src/pages/recipes/RecipesPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import styles from '../../styles/RecipesPage.module.css';
 
 function RecipesPage() {
   return (
     <>
-    <h1>Recipes Page</h1>
-    <p>Display all the recipes.</p>
+      <h1>Recipes Page</h1>
+      <p>Display all the recipes.</p>
 
-    <NavLink to="/recipes/create">
-    <i class="fa-solid fa-plus"></i>
-    </NavLink>
+      <NavLink to="/recipes/create">
+        <div className={styles.ButtonContainer}>
+          <div className={`${styles.Button} ${styles.IconButton}`}>
+            <i className="fa-solid fa-plus"></i>
+          </div>
+        </div>
+      </NavLink>
     </>
-  )
+  );
 }
 
 export default RecipesPage
