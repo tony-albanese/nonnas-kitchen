41aad5e721ce7c396c2745102cf7f0171e24a4e4
tony-albanese <tony.albanese2000@gmail.com>
2023-02-22 15:20:09 +0000

    Implement conditional rendering of add recipe btn.

--- src/pages/recipes/RecipesPage.jsx
+++ src/pages/recipes/RecipesPage.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import styles from '../../styles/RecipesPage.module.css';
+import {useCurrentUser} from "../../contexts/CurrentUserContext";
 
 function RecipesPage() {
+
+  const currentUser = useCurrentUser();
   return (
     <>
       <h1>Recipes Page</h1>
       <p>Display all the recipes.</p>
 
-      <NavLink to="/recipes/create">
+      {currentUser ? (
+        <NavLink to="/recipes/create">
         <div className={styles.ButtonContainer}>
           <div className={`${styles.Button} ${styles.IconButton}`}>
             <i className="fa-solid fa-plus"></i>
           </div>
         </div>
       </NavLink>
+
+      ) : (<></>)}
+
     </>
   );
 }
