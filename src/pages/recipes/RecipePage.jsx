a7b8b5c5dfe7c7d7b6d55f88fc992079ddefbc48
tony-albanese <tony.albanese2000@gmail.com>
2023-02-22 15:19:39 +0000

    Fetch id from parameters

--- src/pages/recipes/RecipePage.jsx
+++ src/pages/recipes/RecipePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 
 function RecipePage() {
+  const { id } = useParams();
   return (
     <>
     <h1>Recipe Page</h1>
-    <p>For the details of a single recipe.</p>
+    <p>For the details of a single recipe id: {id}</p>
     </>
   )
 }
