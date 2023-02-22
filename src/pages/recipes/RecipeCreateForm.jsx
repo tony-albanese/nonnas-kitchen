f20d3af2455b318426dcb45e624c7ff5318afce0
tony-albanese <tony.albanese2000@gmail.com>
2023-02-22 08:53:27 +0000

    Outline layout of form.

--- src/pages/recipes/RecipeCreateForm.jsx
+++ src/pages/recipes/RecipeCreateForm.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 
 function RecipeCreateForm() {
   return (
     <>
-    <h1>RecipeCreateForm</h1>
-    <p>This is where the user can create a recipe.</p>
+      <h1>RecipeCreateForm</h1>
+      <p>This is where the user can create a recipe.</p>
+      <Form>
+        <Container>
+          <Row>
+            <p>Add image. Title. Description. Difficulty. Type.</p>
+
+            <Col md={6}>Image upload goes here.</Col>
+
+            <Col md={6}>
+            
+            </Col>
+          </Row>
+
+
+
+
+          <Row>
+            <p>This row contains the ingredients and steps components.</p>
+          </Row>
+
+
+          <Row>
+            <p>Row for the submit button and cancel button.</p>
+          </Row>
+        </Container>
+      </Form>
     </>
-  )
+  );
 }
 
-export default RecipeCreateForm
+export default RecipeCreateForm;
