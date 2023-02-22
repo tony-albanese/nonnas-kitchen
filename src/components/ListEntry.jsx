86e1f9b0b1f08d0364aaabcf4d3ec06ee681dad4
tony-albanese <tony.albanese2000@gmail.com>
2023-02-22 15:18:32 +0000

    Remove unused import from ListEntry

--- src/components/ListEntry.jsx
+++ src/components/ListEntry.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Row, Button } from "react-bootstrap";
+import { Form, Row} from "react-bootstrap";
 import styles from "../styles/ListEntry.module.css";
 
 function ListEntry({ fields, setFields, label }) {
